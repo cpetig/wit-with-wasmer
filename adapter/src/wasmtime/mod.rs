121887c3dd5ce7633f83a022111fe05ff272b419
Christof Petig <christof.petig@arcor.de>
2023-07-01 18:45:58 +0200

    on the way

--- adapter/src/wasmtime/mod.rs
+++ adapter/src/wasmtime/mod.rs
@@ -1,1 +1,17 @@
 pub mod component;
+
+pub struct Store;
+
+pub trait AsContextMut {
+    type Data;
+
+    fn as_context_mut(&mut self) -> Store { Store{} }
+}
+
+pub use anyhow::Result;
+
+//struct SomeContext<T> ;
+
+impl AsContextMut for Store {
+    type Data = ();
+}
