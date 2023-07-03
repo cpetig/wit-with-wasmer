87243ce59cae40ad60346c7364ecac19a277b537
Christof Petig <christof.petig@arcor.de>
2023-07-03 08:33:47 +0200

    clean up comments

--- wasmer-host/src/main.rs
+++ wasmer-host/src/main.rs
@@ -8,28 +8,24 @@ use wasmer::{imports, Instance, Module, Store, Value};
 wasmtime::component::bindgen!({ path: "../wit/test.wit", world: "testw" });
 
 fn main() -> Result<()> {
-    //let engine = wasmer::Engine::default();
     let mut store = Store::default();
     let engine = store.engine();
     let linker = Linker::new(engine);
 
+    // load module in wasmer
     let module = Module::from_file(&store, "target/wasm32-unknown-unknown/debug/plugin.wasm")?;
-    // The module doesn't import anything, so we create an empty import object.
-    // let import_object = imports! {};
-    // let instance = Instance::new(&mut store, &module, &import_object)?;
 
+    // create an adapter
     let component = Component::from_binary(engine, &module)?;
 
+    // instantiate the world using both
     let (myworld, _) = Testw::instantiate(&mut store, &component, &linker)?;
 
-    // Now let's use  the exported render function
+    // call the exported function
     let res = myworld.call_testfun(&mut store, "hello")?;
 
     assert_eq!(res, "hello world");
     println!("Out: {}", res);
 
-    // let add_one = instance.exports.get_function("testfun")?;
-    // let result = add_one.call(&mut store, &[Value::I32(0), Value::I32(0)])?;
-    // println!("Result {result:?}");
     Ok(())
 }
